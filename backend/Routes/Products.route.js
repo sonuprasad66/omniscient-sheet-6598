ea7c5a9ef5a0661eb45e522e19f8cb29d3da5273
sonuprasad66 <sp.srp1999@gmail.com>
2022-11-12 12:10:14 +0530

    env_final

--- backend/Routes/Products.route.js
+++ backend/Routes/Products.route.js
@@ -2,25 +2,6 @@ const { Router } = require("express");
 const { ProductModel } = require("../Models/Products.model");
 const ProductRouter = Router();
 
-// ProductRouter.get("/alldata", async (req, res) => {
-//   let { brand } = req.query;
-//   console.log("bbbb", brand);
-
-//   if (brand && brand !== undefined) {
-//     let bag = [];
-//     brand.map(async (item) => {
-//       let data = await ProductModel.find({ brand: item });
-//       bag = [...bag, data];
-//     });
-//     res.send({ data: bag, message: "request successfull" });
-//   } else {
-//     let data = await ProductModel.find({});
-//     res.send({ data: data, message: "request successfull" });
-//   }
-// });
-
-// db.info.find( { contribs: { $in: [ "ALGOL", "Lisp" ]} } ).pretty()
-
 ProductRouter.get("/alldata", async (req, res) => {
   let q = req.query;
   const arr = q.brand;
