8c2dc77d8ab4ccb698e5239c9018f51b8e3b5a77
sonuprasad66 <sp.srp1999@gmail.com>
2022-11-13 15:30:57 +0530

    final

--- frontend/src/Redux/Product/action.js
+++ frontend/src/Redux/Product/action.js
@@ -5,7 +5,7 @@ export const getProducts = (params) => (dispatch) => {
   // console.log("action", params);
   dispatch({ type: types.GET_PRODUCTS_REQUEST });
   return axios
-    .get(`http://localhost:8080/alldata`, params)
+    .get(`https://tata1mgbd-production.up.railway.app/alldata`, params)
     .then((res) => {
       // console.log(res.data);
       return dispatch({ type: types.GET_PRODUCTS_SUCCESS, payload: res.data });
